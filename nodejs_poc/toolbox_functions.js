6d73e95a845d471426373f1bff0804b127130d02
zhang.yifei <yifei.z.luiz@gmail.com>
2018-09-27 14:19:41 -0300

    toolbox commands code base

---
+++ nodejs_poc/toolbox_functions.js
@@ -0,0 +1,38 @@
+// Engine generate command module
+'use strict';
+
+var imageName = 'marvinaiplatform/marvin-automl:0.0.1'
+var containerName = 'docker-api-test'
+
+const {Docker} = require('node-docker-api');
+
+// Log capture function
+const promisifyStream = stream => new Promise((resolve, reject) => {
+  stream.on('data', data => console.log(data.toString()))
+  stream.on('end', resolve)
+  stream.on('error', reject)
+});
+
+const docker = new Docker({ socketPath: '/var/run/docker.sock' });
+let _container;
+
+// Start container, run engine generate command and capture logs
+docker.container.create({
+  Image: imageName,
+  Cmd: [ '/bin/bash', '-c', 'tail -f /var/log/dmesg' ],
+  name: containerName
+})
+  .then(container => container.start())
+  .then(container => {
+  	_container = container
+  	return container.exec.create({
+  		AttachStdout: true,
+  		AttachStderr: true,
+  		Cmd: ['workon python-toolbox-env', 'marvin engine-generate']
+  	})
+  })
+  .then(stream => promisifyStream(stream))
+  .catch(error => console.log(error));
+
+  ### TO DO ###
+  // User instructions for inserting project name, maintainer name and etc....
