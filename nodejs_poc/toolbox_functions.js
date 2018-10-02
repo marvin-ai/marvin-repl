ee13a1dbb0259226ed74e4fb0f345a32e9fce4dd
zhang.yifei <yifei.z.luiz@gmail.com>
2018-10-02 11:47:43 -0300

    notebook docker sdk function

--- nodejs_poc/toolbox_functions.js
+++ nodejs_poc/toolbox_functions.js
@@ -1,38 +1,41 @@
 // Engine generate command module
 'use strict';
 
+const {Docker} = require('node-docker-api');
+const docker = new Docker({ socketPath: '/var/run/docker.sock' });
+
 var imageName = 'marvinaiplatform/marvin-automl:0.0.1'
 var containerName = 'docker-api-test13'
 
-const {Docker} = require('node-docker-api');
+function notebookSDK(imageName, containerName) {
 
-// Log capture function
-const promisifyStream = stream => new Promise((resolve, reject) => {
-  stream.on('data', data => console.log(data.toString()))
-  stream.on('end', resolve)
-  stream.on('error', reject)
-});
+	let _container;
 
-const docker = new Docker({ socketPath: '/var/run/docker.sock' });
-let _container;
+	// Log capture
+	const promisifyStream = stream => new Promise((resolve, reject) => {
+		stream.on('data', data => console.log(data.toString()))
+		stream.on('end', resolve)
+		stream.on('error', reject)
+	});
 
-// Start container, run engine generate command and capture logs
-docker.container.create({
-  Image: imageName,
-  Cmd: [ '/bin/bash', '-c', 'tail -f /var/log/dmesg' ],
-  name: containerName
-})
-  .then(container => container.start())
-  .then(container => {
-  	_container = container
-  	return container.exec.create({
-      	  AttachStdout: true,
-      	  AttachStderr: true,
-      	  Cmd: [ '/bin/bash', '-c', 'source /usr/local/bin/virtualenvwrapper.sh ; workon marvin-engine-env ; cd /opt/marvin/engine/ ; marvin notebook --allow-root -p 9999 ' ]
-    })
-  })
-  .then(exec => {
-    return exec.start({ Detach: false })
-  })
-  .then(stream => promisifyStream(stream))
-  .catch(error => console.log(error));
+	// Start container, run engine generate command and capture logs
+	docker.container.create({
+		Image: imageName,
+		Cmd: [ '/bin/bash', '-c', 'tail -f /var/log/dmesg' ],
+		name: containerName
+	})
+	.then(container => container.start())
+	.then(container => {
+		_container = container
+		return container.exec.create({
+			AttachStdout: true,
+			AttachStderr: true,
+			Cmd: [ '/bin/bash', '-c', 'source /usr/local/bin/virtualenvwrapper.sh ; workon marvin-engine-env ; cd /opt/marvin/engine/ ; marvin notebook --allow-root -p 9999 ' ]
+		})
+	})
+	.then(exec => {
+		return exec.start({ Detach: false })
+	})
+	.then(stream => promisifyStream(stream))
+	.catch(error => console.log(error));
+}
