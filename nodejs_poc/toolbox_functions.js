9bd8a2ce543d73afafc2e3c3a1a8af15fdd11f06
zhang.yifei <yifei.z.luiz@gmail.com>
2018-10-02 11:52:42 -0300

    engine-generate SDK

--- nodejs_poc/toolbox_functions.js
+++ nodejs_poc/toolbox_functions.js
@@ -4,22 +4,8 @@ const docker = new Docker({ socketPath: '/var/run/docker.sock' })
 var imageName = 'marvinaiplatform/marvin-automl:0.0.1'
 var containerName = 'docker-api-test'
 
-// Management of docker container.
-function containerManagement(imageName, containerName) {
-	docker.container.create({
-		Image: imageName,
-		name: containerName
-	})
-	  .then((container) => container.start())
-	  // .then((container) => container.stop())
-	  // .then((container) => container.restart())
-	  // .then((container) => container.delete({ force: true }))
-	  .catch((error) => console.log(error))
-}
-
-
-// Create and get container's logs.
-function containerLogs(imageName, containerName) {
+// Engine-generate command here is mapped to a simple container start up.
+function engineGenerateSDK(imageName, containerName) {
 	docker.container.create({
 		Image: imageName,
 		name: containerName
